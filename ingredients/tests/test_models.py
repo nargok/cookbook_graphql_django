0e338409ccb8dcf7fbe0bc2971d45bd1453e9655
nargok <nargok@gmail.com>
2018-11-28 17:10:16 +0900

    Added test case for Ingredient object save successfully

--- ingredients/tests/test_models.py
+++ ingredients/tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-from ingredients.models import Category
+from ingredients.models import Category, Ingredient
 
 """
 pytestにDBアクセスさせるにはこの設定が必要
@@ -10,8 +10,22 @@ pytestmark = pytest.mark.django_db
 
 class TestCategoryModel:
 
+
   def test_category_save(self):
     category = Category.objects.create(name="Category01")
 
     saved_category = Category.objects.get(pk=category.id)
     assert saved_category.name == "Category01"
+
+class TestIngredientModel:
+
+  def test_ingredient_save(self):
+    category = Category.objects.create(name="Category01")
+    ingredient = Ingredient.objects.create(
+      name="Ingredient01",
+      notes="TestNotes",
+      category=category
+    )
+
+    saved_ingredient = Ingredient.objects.get(pk=ingredient.id)
+    assert saved_ingredient.name == "Ingredient01"
